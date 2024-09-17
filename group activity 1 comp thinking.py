fc0ef7df4873e76d0793c5c4da5769065dc03192
connorboger1027 <connor27@outlook.com>
2024-09-17 15:29:34 -0500

    Add files via upload
    Work on the group assignment here!

---
+++ group activity 1 comp thinking.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 17:56:32 2024
+
+@author: conno
+"""
+
+# MindTap 1.4 Homework - Circle
+
+
+interest_rate = 0.075
+
+if interest_rate < 0.05:
+    print("The interest rate is low")
+elif interest_rate >= .07:
+    print("This interest rate is very high.")
+    
+    
+mfj = input("Are you married filing jointly? - Yes or No")
+
+#Federal
+if mfj == "Yes":
+    income = income - standard_deduction
+    
+    
+    
