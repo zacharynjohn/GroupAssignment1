fc1cbc7c58a1ea25e68fa9ae1ddf3e151c9020d8
connorboger1027 <connor27@outlook.com>
2024-09-18 14:07:24 -0500

    Update group activity 1 comp thinking.py

--- group activity 1 comp thinking.py
+++ group activity 1 comp thinking.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 """
+Coders: Connor Boger, Noelle Gallagher, Quinn Geiger, Zach Johnson
 Variables:
+- income --> User's income for the year
+- partner_income --> Partner's income for the year
+- mfj --> Total household income for User and Partner
+
 Inputs
 Outputs
 """
 
+# interest_rate = 0.075
 
-interest_rate = 0.075
-
-if interest_rate < 0.05:
+# if interest_rate < 0.05:
     print("The interest rate is low")
-elif interest_rate >= .07:
+# elif interest_rate >= .07:
     print("This interest rate is very high.")
-    
+
+income = float(input("What is your income for this year?")
     
 mfj = input("Are you married filing jointly? - Yes or No")
+if mfj == "Yes":
+   partner_income = int(input("What is your partner's income for the year?")
+
 
 #Federal
 if mfj == "Yes":
